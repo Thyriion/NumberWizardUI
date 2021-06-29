5aad00c04640b4bcd9056a2e727334b27975ab1c
Thyriion <kevinschueler2000@gmail.com>
2021-06-29 19:59:31 +0200

    initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] private Int32 Max;
+    [SerializeField] private Int32 Min;
+    [SerializeField] private TextMeshProUGUI GuessText;
+
+    private Int32 Guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        UpdateGuess();
+    }
+
+    void UpdateGuess()
+    {
+        Guess = UnityEngine.Random.Range(Min, Max + 1);
+        GuessText.text = Guess.ToString();
+    }
+
+    public void OnPressHigher()
+    {
+        Min = Guess + 1;
+        UpdateGuess();
+    }
+
+    public void OnPressLower()
+    {
+        Max = Guess - 1;
+        UpdateGuess();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        var startSceneIndex = 0;
+        SceneManager.LoadScene(startSceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
